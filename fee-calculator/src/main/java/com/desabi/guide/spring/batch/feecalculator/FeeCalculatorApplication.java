c07410d61ba5850ecc6a015cd23502733c6397c0
desabi <desabi@live.com>
2026-04-21 07:43:32 -0600

    feat: fee calculator, add base project

---
+++ fee-calculator/src/main/java/com/desabi/guide/spring/batch/feecalculator/FeeCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.desabi.guide.spring.batch.feecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeeCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeeCalculatorApplication.class, args);
+	}
+
+}
---
+++ fee-calculator/src/test/java/com/desabi/guide/spring/batch/feecalculator/FeeCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desabi.guide.spring.batch.feecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeeCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
