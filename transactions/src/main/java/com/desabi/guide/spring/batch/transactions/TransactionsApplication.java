930d841334b81be22eaf220347a36ba2cddc9c48
desabi <desabi@live.com>
2026-04-25 17:18:14 -0600

    feat: transactions, add base project

---
+++ transactions/src/main/java/com/desabi/guide/spring/batch/transactions/TransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.desabi.guide.spring.batch.transactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionsApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(TransactionsApplication.class, args);
+	}
+
+}
---
+++ transactions/src/test/java/com/desabi/guide/spring/batch/transactions/TransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desabi.guide.spring.batch.transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
