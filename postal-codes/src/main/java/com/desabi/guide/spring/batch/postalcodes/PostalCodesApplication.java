4f35bc5a59f5ecb53ebd61859e34a4bf067c09b2
desabi <desabi@live.com>
2026-04-25 13:23:21 -0600

    feat: postal codes, add base project

---
+++ postal-codes/src/main/java/com/desabi/guide/spring/batch/postalcodes/PostalCodesApplication.java
@@ -0,0 +1,13 @@
+package com.desabi.guide.spring.batch.postalcodes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostalCodesApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(PostalCodesApplication.class, args);
+	}
+
+}
---
+++ postal-codes/src/test/java/com/desabi/guide/spring/batch/postalcodes/PostalCodesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desabi.guide.spring.batch.postalcodes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostalCodesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
